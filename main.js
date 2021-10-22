18e8b1540c30bf1bbf03fade363efc8b9225696f
Graziele D. Sena <62073770+grazuzu@users.noreply.github.com>
2021-10-22 20:00:33 -0300

    Update data.bio

--- main.js
+++ main.js
@@ -25,7 +25,7 @@
         .then(response => response.json()) 
         .then(data => {
           userName.textContent = data.name
-          userBio.textContent = data.userBio
+          userBio.textContent = data.bio
           userLink.href = data.html_url
           userImage.src = data.avatar_url
           userLogin.textContent = data.login
